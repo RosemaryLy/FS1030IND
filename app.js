ba65e5fc307dde134911500385acc7fed21519d0
Rosemary <rosey.ly@hotmail.com>
2020-04-23 12:27:44 -0400

    mysql connect

---
+++ app.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const mysql = require('mysql');
+const passport = require('passport');
+const flash = require('connect-flash');
+const session = require('express-session');
+
+const app = express();
+
+// Passport Config
+require('./config/passport')(passport);
+
+// DB Config
+const db = require('./config/keys').mongoURI;
+
+// Connect to mySQL
+var connection = mysql.createConnection({
+	host:'localhost',
+	user:'USERNAME_HERE',
+	password:'PASSWORD_HERE',
+	database:'DATABASE_NAME_HERE'
+});
+connection.connect(function(error){
+	if(!!error) {
+		console.log(error);
+	} else {
+		console.log('Connected..!');
+	}
+});
+
+module.exports = connection;
+
+// EJS
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
+
+// Express body parser
+app.use(express.urlencoded({ extended: true }));
+
+// Express session
+app.use(
+  session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+  })
+);
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Connect flash
+app.use(flash());
+
+// Global variables
+app.use(function(req, res, next) {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  next();
+});
+
+// Routes
+app.use('/', require('./routes/index.js'));
+app.use('/users', require('./routes/users.js'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
