2b684d0d123b8c5f8f2c9bc14aa662dd4daecb27
Rosemary <rosey.ly@hotmail.com>
2020-04-24 10:54:27 -0400

    connection error

--- app.js
+++ app.js
@@ -64,6 +64,6 @@ app.use(function(req, res, next) {
 app.use('/', require('./routes/index.js'));
 app.use('/users', require('./routes/users.js'));
 
-const PORT = process.env.PORT || 3306;
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
