2ab43bc05984c3e629218848e94e19fdd9dd6811
Rosemary <rosey.ly@hotmail.com>
2020-04-25 02:09:46 -0400

    -

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 
 const {getHomePage} = require('./routes/index');
 const {addPortfolioPage, addPortfolio, deletePortfolio, editPortfolio, editPortfolioPage} = require('./routes/player');
-const port = 2000;
+const port = 5000;
 
 // create connection to database
 // the mysql.createConnection function takes in a configuration object which contains host, user, password and the database name.
