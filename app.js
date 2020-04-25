87050184f6a413b444cedfdee8de6128398036be
Rosemary <rosey.ly@hotmail.com>
2020-04-24 21:28:03 -0400

    starting fresh

--- app.js
+++ app.js
@@ -1,87 +1,51 @@
 const express = require('express');
-const expressLayouts = require('express-ejs-layouts');
-const mysql = require('mysql');
+const fileUpload = require('express-fileupload');
 const bodyParser = require('body-parser');
-const passport = require('passport');
-const flash = require('connect-flash');
-const session = require('express-session');
+const mysql = require('mysql');
+const path = require('path');
 const app = express();
 
 const {getHomePage} = require('./routes/index');
-const {addPortfolio, addPortfolioPage, DeletePortfolio, editPortfolio, editPortfolioPage} = require ('./routes/Portfolio');
-const {addResume, addResumePage, DeleteResume, editResume, editResumePage} =require('./routes/Resume')
+const {addPortfolioPage, addPortfolio, deletePortfolio, editPortfolio, editPortfolioPage} = require('./routes/Portfolio');
+const port = 8000;
 
-const PORT = process.env.PORT || 8000;
-
-
-
-// Connect to mySQL
-let connection = mysql.createConnection({
-	host:'localhost',
-	user:'nodeclient',
-	password:'123456',
-	database:'fs1030project'
-});
-connection.connect(function(error){
-	if(!!error) {
-		console.log(error);
-	} else {
-		console.log('Connected..!');
-	}
+// create connection to database
+// the mysql.createConnection function takes in a configuration object which contains host, user, password and the database name.
+const db = mysql.createConnection ({
+    host: 'localhost',
+    user: 'nodeclient',
+    password: '123456',
+    database: 'fs1030'
 });
 
-module.exports = connection;
+// connect to database
+db.connect((err) => {
+    if (err) {
+        throw err;
+    }
+    console.log('Connected!');
+});
+global.db = db;
 
-// EJS
-app.use(expressLayouts);
-app.set('view engine', 'ejs');
-
-// Express body parser
-app.use(express.urlencoded({ extended: true }));
+// configure middleware
+app.set('port', process.env.port || port); // set express to use this port
+app.set('views', __dirname + '/views'); // set express to look in this folder to render our view
+app.set('view engine', 'ejs'); // configure template engine
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json()); // parse form data client
 app.use(express.static(path.join(__dirname, 'public'))); // configure express to use public folder
 app.use(fileUpload()); // configure fileupload
-// Express session
-app.use(
-  session({
-    secret: 'secret',
-    resave: true,
-    saveUninitialized: true
-  })
-);
 
-// Passport middleware
-app.use(passport.initialize());
-app.use(passport.session());
+// routes for the app
 
-// Connect flash
-app.use(flash());
-
-// Global variables
-app.use(function(req, res, next) {
-  res.locals.success_msg = req.flash('success_msg');
-  res.locals.error_msg = req.flash('error_msg');
-  res.locals.error = req.flash('error');
-  next();
-});
-
-// Routes
 app.get('/', getHomePage);
 app.get('/add', addPortfolioPage);
 app.get('/edit/:id', editPortfolioPage);
-app.get('/delete/:id', DeletePortfolio);
+app.get('/delete/:id', deletePortfolio);
 app.post('/add', addPortfolio);
 app.post('/edit/:id', editPortfolio);
 
-
-app.get('/add', addResumePage);
-app.get('/edit/:id', editResumePage);
-app.get('/delete/:id', DeleteResume);
-app.post('/add', addResume);
-app.post('/edit/:id', editResume);
-
-
-
-//set the app to listen on the port
-app.listen(PORT, console.log(`Server started on port ${PORT}`));
+// set the app to listen on the port
+app.listen(port, () => {
+    console.log(`Server running on port: ${port}`);
+});
