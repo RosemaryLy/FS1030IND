4d263738ef18f643b4a1d47c1df3ff965cedc219
Rosemary <rosey.ly@hotmail.com>
2020-04-25 19:21:20 -0400

    add contact listing page

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var portfoliosRouter = require('./routes/portfolios');
 var resumeRouter= require ('./routes/resume');
+var contactlistingRouter= require('./routes/contactlisting')
 
 var app = express();
 const port = 5000;
@@ -42,6 +43,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/portfolios', portfoliosRouter);
 app.use('/resume', resumeRouter);
+app.use('contactlisting', contactlistingRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/contactlisting.js
+++ routes/contactlisting.js
@@ -2,18 +2,18 @@ var express = require('express');
 var router = express.Router();
 var dbConn  = require('../config/db');
  
-// display portfolios page
+// display contactlisting page
 router.get('/', function(req, res, next) {
       
-    dbConn.query('SELECT * FROM portfolios ORDER BY id desc',function(err,rows)     {
+    dbConn.query('SELECT * FROM contactsfile ORDER BY id desc',function(err,rows)     {
  
         if(err) {
             req.flash('error', err);
             // render to views/portfolios/index.ejs
-            res.render('portfolios',{data:''});   
+            res.render('contactlisting',{data:''});   
         } else {
             // render to views/portfolios/index.ejs
-            res.render('portfolios',{data:rows});
+            res.render('contactlisting',{data:rows});
         }
     });
 });
